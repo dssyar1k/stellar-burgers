e6cc937c28769165840c75a16cb943b529894374
dssyar1k <y.pavlov@inbox.ru>
2026-02-01 13:49:27 +0300

    Добавление тестов Jest

--- src/services/ingredientsSlice.ts
+++ src/services/ingredientsSlice.ts
@@ -8,7 +8,7 @@ export type TStateIngredients = {
   error: string | null;
 };
 
-const initialState: TStateIngredients = {
+export const initialState: TStateIngredients = {
   ingredients: [],
   loading: false,
   error: null
